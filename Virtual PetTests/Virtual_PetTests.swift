e2f7577ff8628ff1b25bb04c08d81468c4ddae45
Juila Li <lijulia@wustl.edu>
2018-06-23 16:40:15 -0500

    Initial Commit

---
+++ Virtual PetTests/Virtual_PetTests.swift
@@ -0,0 +1,36 @@
+//
+//  Virtual_PetTests.swift
+//  Virtual PetTests
+//
+//  Created by Julia Li on 6/23/18.
+//  Copyright © 2018 Julia Li. All rights reserved.
+//
+
+import XCTest
+@testable import Virtual_Pet
+
+class Virtual_PetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
